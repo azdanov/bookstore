fa05d102608cd8e9b5eeb6769222e4e494171ac8
Anton Ždanov <anton@azdanov.dev>
2024-04-27 19:01:29 +0300

    add catalog-service product migrations

---
+++ catalog-service/src/main/resources/db/migration/V1__create_products_table.sql
@@ -0,0 +1,14 @@
+create sequence product_id_seq start with 1 increment by 50;
+
+create table products
+(
+    id          bigint                   default nextval('product_id_seq') not null,
+    code        text                                                       not null unique,
+    name        text                                                       not null,
+    description text,
+    image_url   text,
+    price       numeric                                                    not null check (price >= 0),
+    created_at  timestamp with time zone default current_timestamp         not null,
+    updated_at  timestamp with time zone default current_timestamp         not null,
+    primary key (id)
+);
